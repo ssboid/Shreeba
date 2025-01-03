68ebf1722edae016d5c76e2623056e8faf13274a
Sunit Shakya <shakyasunit11@gmail.com>
2025-01-04 00:05:34 +0545

    updated homepage and network project access

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,13 @@
 import React, { useRef } from "react";
 import BannerIso from "../assets/home/banneriso.png";
-import { FaPencilAlt } from "react-icons/fa";
+import {
+  FaPencilAlt,
+  FaTrashAlt,
+  FaSearch,
+  FaPlusCircle,
+  FaFilter,
+  FaTag,
+} from "react-icons/fa";
 
 const Homepage = () => {
   // Refs to each div
@@ -68,21 +75,22 @@ const Homepage = () => {
       {/* Div1 */}
       <div
         ref={div1Ref}
-        className="h-screen w-full  flex flex-col md:flex-row items-center justify-center bg-cover bg-center"
+        className="h-screen -mb-32 lg:-mb-16 w-full  flex flex-col md:flex-row items-center justify-center bg-cover bg-center"
         style={{
           backgroundImage: "url('src/assets/brand/background_mirrored.png')",
         }}
       >
         <div className=" md:h-full w-full md:w-1/2  flex flex-col p-6 xl:p-[8.33%] xl:py-[12%] items-start justify-start space-y-12">
-          <h1 className="font-heading font-bold text-5xl text-primary1000">Easy Inventory Management</h1>
+          <h1 className="font-heading font-bold text-5xl text-primary1000">
+            Easy Inventory Management
+          </h1>
           <p className="font-body text-xl">
             Manage your store easily. Add, edit, and track items with a few
-            clicks.
-            Log in now and simplify your work today!
+            clicks. Log in now and simplify your work today!
           </p>
           <button
             type="button"
-            className="bg-primaryOrange text-white rounded-full px-4 py-2 hover:bg-opacity-90"
+            className="bg-primaryOrange text-white rounded-full px-6 py-4 hover:bg-opacity-90"
           >
             Get Started
           </button>
@@ -95,12 +103,14 @@ const Homepage = () => {
       {/* Div2 */}
       <div
         ref={div2Ref}
-        className="h-screen w-full bg-sky-200 flex flex-col md:flex-row items-center justify-center"
+        className=" w-full bg-orange-100 flex flex-col md:flex-row items-center justify-center py-12 pb-16 lg:py-0 "
       >
-        <div className="h-screen md:h-full w-full md:w-1/2 bg-orange-100 flex flex-col p-6 xl:p-[8.33%]  flex items-start justify-start space-y-12">
+        <div className="md:h-full w-full md:w-1/2  flex flex-col p-6 pt-0   xl:p-[8.33%]  flex items-start justify-start space-y-12">
           <div className="space-y-2">
-            <span>FOR YOUR BUSINESS NEED</span>
-            <h2 className="font-heading font-bold text-primary1000 text-4xl">Easy Inventory Management</h2>
+            <span className="text-primaryOrange">FOR YOUR BUSINESS NEED</span>
+            <h2 className="font-heading font-bold text-primary1000 text-4xl">
+              Easy Inventory Management
+            </h2>
           </div>
           <span>
             By providing a seamless way to add, edit, delete, and view inventory
@@ -112,56 +122,153 @@ const Homepage = () => {
             through unique codes.
           </span>
         </div>
-        <div className="hidden md:flex h-screen md:h-full w-full md:w-1/2 bg-green-500 items-center justify-center">
-          <h1 className="text-5xl">2B</h1>
+        <div className=" md:flex md:h-full w-full md:w-1/2 items-center xl:p-[8.33%] justify-center px-24 lg:px-32">
+          <div className="flex  space-x-8 w-full justify-center ">
+            <div className="flex mx-6 lg:mx-0 space-x-4 justify-center">
+              <div className="bg-black lg:w-52 lg:h-96 flex items-center justify-center mt-16 rounded-full shadow-[0_16px_0_var(--tw-shadow-color)] shadow-primaryOrange w-40 h-72">
+                QW
+              </div>
+              <div className="bg-white lg:w-52 lg:h-96 flex items-center justify-center mb-16 rounded-full shadow-[0_16px_0_var(--tw-shadow-color)] shadow-primaryOrange w-40 h-72">
+                EQ
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
       {/* Div3 */}
       <div
         ref={div3Ref}
-        className="h-screen w-full flex flex-col items-center justify-start p-6 xl:p-[8.33%] bg-secondary100 space-y-24"
+        className=" w-full flex flex-col items-center justify-start p-6 py-12 pb-16 lg:py-0 xl:p-[8.33%] bg-secondary100 space-y-16"
       >
-        <div className="lex flex-col space-y-2 items-center justify-center text-center">
-          <span>COMPLETE INVENTORY CONTROL</span>
-          <h2 className="text-4xl">What We Do</h2>
+        <div className="flex flex-col space-y-2 items-center justify-center text-center">
+          <span className="text-primaryOrange">COMPLETE INVENTORY CONTROL</span>
+          <h2 className="font-heading font-bold text-primary1000 text-4xl">
+            What does it Do?
+          </h2>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-24">
-          <div
-            className="bg-gray-200 rounded shadow flex items-center justify-center space-x-6
-          "
-          >
-            <div className=""><FaPencilAlt className="text-2xl text-primaryOrange" /></div>
-            <div className="space-y-2">
-              <div>Add Records</div>
-              <div>Easily input new inventory details</div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-18 gap-y-16">
+          {[
+            {
+              title: "Add Records",
+              description: "Easily input new inventory details",
+              icon: <FaPlusCircle className="text-3xl text-primaryOrange" />,
+            },
+            {
+              title: "Edit Records",
+              description: "Update existing records with new information",
+              icon: <FaPencilAlt className="text-3xl text-primaryOrange" />,
+            },
+            {
+              title: "View",
+              description: "Access all inventory details in one place",
+              icon: <FaSearch className="text-3xl text-primaryOrange" />,
+            },
+            {
+              title: "Delete Records",
+              description: "Remove items that are no longer needed",
+              icon: <FaTrashAlt className="text-3xl text-primaryOrange" />,
+            },
+            {
+              title: "Generate Unique Codes",
+              description:
+                "Create a unique code for each item using entered details",
+              icon: <FaTag className="text-3xl text-primaryOrange" />,
+            },
+            {
+              title: "Filter Records",
+              description:
+                "Search through inventory with various filters for quick access",
+              icon: <FaFilter className="text-3xl text-primaryOrange" />,
+            },
+            {
+              title: "Record and Track Sales",
+              description:
+                "Mark items as sold by searching with their unique code",
+              icon: <FaTag className="text-3xl text-primaryOrange" />,
+            },
+          ].map((item, index) => (
+            <div
+              key={index}
+              className={`flex flex-col items-center px-8 text-center ${
+                index === 6
+                  ? "md:col-span-3 md:flex md:justify-center"
+                  : "md:flex-1 px-8"
+              }`}
+            >
+              <div className="mb-4">{item.icon}</div>
+              <div className="space-y-2">
+                <div className="font-heading font-semibold text-primary1000">
+                  {item.title}
+                </div>
+                <div className="font-body text-neutral700 ">
+                  {item.description}
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="bg-gray-200 p-4 rounded shadow">Item 2</div>
-          <div className="bg-gray-200 p-4 rounded shadow">Item 3</div>
-          <div className="bg-gray-200 p-4 rounded shadow">Item 4</div>
-          <div className="bg-gray-200 p-4 rounded shadow">Item 5</div>
-          <div className="bg-gray-200 p-4 rounded shadow">Item 6</div>
-          <div className="bg-gray-200 p-4 rounded shadow md:col-span-3 md:mx-auto">
-            Item 7
-          </div>
+          ))}
         </div>
       </div>
 
       {/* Div4 */}
       <div
         ref={div4Ref}
-        className="h-screen w-full bg-sky-400 flex items-center justify-center"
+        className="bg-orange-100 lg:min-h-screen space-y-16 w-full flex flex-col flex p-6 xl:p-[8.33%] py-12 pb-16 lg:py-0 "
       >
-        <h1 className="text-9xl">4</h1>
+        <div className="space-y-2">
+          <span className="text-primaryOrange">WE'RE HERE TO HELP</span>
+          <h2 className="flex flex-col font-heading font-bold text-primary1000 text-4xl space-y-2">
+            <div>Stuck Somewhere?</div>
+            <div>Contact our team!</div>
+          </h2>
+        </div>
+        <div>
+          <div className="flex flex-col lg:flex-row lg:mx-0 lg:space-x-4 space-y-16 lg:space-y-0 lg:w-full lg:justify-evenly">
+            <div className="bg-black lg:w-52 lg:h-96 flex items-center justify-center rounded-full shadow-[0_16px_0_var(--tw-shadow-color)] shadow-primaryOrange w-40 h-72">
+              QW
+            </div>
+            <div className="bg-white lg:w-52 lg:h-96 flex items-center justify-center rounded-full shadow-[0_16px_0_var(--tw-shadow-color)] shadow-primaryOrange w-40 h-72">
+              EQ
+            </div>
+            <div className="bg-black lg:w-52 lg:h-96 flex items-center justify-center rounded-full shadow-[0_16px_0_var(--tw-shadow-color)] shadow-primaryOrange w-40 h-72">
+              QW
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Div5 */}
       <div
         ref={div5Ref}
-        className="h-screen w-full bg-sky-500 flex items-center justify-center"
+        className="w-full flex flex-col items-center justify-start p-6 py-12 pb-16 lg:py-0 xl:p-[8.33%] bg-secondary100 space-y-16"
+        style={{
+          backgroundImage: "url('src/assets/brand/background.png')",
+          backgroundSize: "cover", // Ensures the background image covers the entire container
+          backgroundPosition: "center", // Ensures the background is centered
+          backgroundRepeat: "no-repeat", // Prevents the background from repeating
+        }}
       >
-        <h1 className="text-9xl">5</h1>
+        <div className="text-center flex flex-col items-center justify-center space-y-12">
+          <div className="flex flex-col space-y-2">
+            <span className="text-primaryOrange text-center">OUR PURPOSE</span>
+            <h2 className="flex flex-col font-heading font-bold text-primary1000 text-4xl space-y-2 text-center">
+              Manage Smarter Today!
+            </h2>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <span>
+              Ready to take control of your inventory? Log in now to get
+              started!
+            </span>
+            <button
+              type="button"
+              className="bg-primaryOrange text-white rounded-full px-6 py-4 hover:bg-opacity-90 self-center" // Added self-center to center the button
+            >
+              Log In
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        borderDefault: '#BBC2C9',
-        primaryOrange: '#DE761C',
-        primary100: '#FBECDE',
-        primary1000: '#59300B',
-        secondary100: '#FAFAEE',
-        
+        borderDefault: "#BBC2C9",
+        primaryOrange: "#DE761C",
+        primary100: "#FBECDE",
+        primary1000: "#59300B",
+        secondary100: "#FAFAEE",
+        neutral700: "#303030",
       },
 
       fontFamily: {
-        heading: ['Roboto', 'sans-serif'], // Heading font
-        body: ['Nunito Sans', 'sans-serif'], // Body font
+        heading: ["Roboto", "sans-serif"], // Heading font
+        body: ["Nunito Sans", "sans-serif"], // Body font
       },
 
-      
       spacing: {
-        '72': '18rem',  // 72px spacing
-        '80': '20rem',  // 80px spacing
-        '96': '24rem',  // 96px spacing
+        72: "18rem", // 72px spacing
+        80: "20rem", // 80px spacing
+        96: "24rem", // 96px spacing
       },
     },
   },
   plugins: [
     function ({ addBase }) {
       addBase({
-        '#root': {
-          padding: '0', // example padding removal
-          backgroundColor: '#f0f0f0', // example background color
-          minHeight: '100vh', // example full screen height
-          width: '100%', // ensures full width
-          maxWidth: 'none', // disables max-width
+        "#root": {
+          padding: "0", // example padding removal
+          backgroundColor: "#f0f0f0", // example background color
+          minHeight: "100vh", // example full screen height
+          width: "100%", // ensures full width
+          maxWidth: "none", // disables max-width
         },
       });
     },
   ],
-}
+};
